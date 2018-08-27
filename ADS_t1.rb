7735a8e53fb42e88888d18ff0392e8c044f8d108
Matthias Nunes <matthiasnunes@gmail.com>
2018-08-27 19:38:21 -0300

    Retabs file

--- ADS_t1.rb
+++ ADS_t1.rb
@@ -3,52 +3,52 @@ include Containers
 
 class Queue
 
-	attr_reader :server_count, :size, :client_count
+  attr_reader :server_count, :size, :client_count
 
-	def initialize(server_count, size)
-		@server_count = server_count
-		@size = size
-		@client_count = 0
-	end
+  def initialize(server_count, size)
+    @server_count = server_count
+    @size = size
+    @client_count = 0
+  end
 
-	def increment
-		@client_count = client_count + 1 if @client_count < @size
-	end
+  def increment
+    @client_count = client_count + 1 if @client_count < @size
+  end
 
-	def decrement
-		@client_count = client_count - 1 if @client_count > 0
-	end
+  def decrement
+    @client_count = client_count - 1 if @client_count > 0
+  end
 
-	def to_s
-		"Queue\n" + 
-		"server_count: #{@server_count}\n" +
-		"size: #{@size}\n" +
-		"client_count: #{@client_count}"
-	end
+  def to_s
+    "Queue\n" +
+    "server_count: #{@server_count}\n" +
+    "size: #{@size}\n" +
+    "client_count: #{@client_count}"
+  end
 
 end
 
 def randon_number
-	rand
+  rand
 end
 
 class Simulation
 
-	def initialize(server_count, size)
-		@queue = Queue.new(server_count, size)
- 		@statistics = {}
- 		size.times { | i | @statistics[i] = 0 }
-	end
+  def initialize(server_count, size)
+    @queue = Queue.new(server_count, size)
+    @statistics = {}
+    size.times { | i | @statistics[i] = 0 }
+  end
 
-	def to_s
-		@queue.to_s +
-		"\n" +
-		@statistics.to_s
-	end
+  def to_s
+    @queue.to_s +
+    "\n" +
+    @statistics.to_s
+  end
 
-	def run
-		
-	end
+  def run
+
+  end
 
 end
 
